cc23d5ed5b42cbb4ecc4f975ac08148281f0c68f
Carlos Saito <c.saito.style@gmail.com>
2017-02-08 13:17:29 +0100

    New getContent API (see description below)
    The new getContent API allows getting content from any source (not only one
    media). It automatically detects what parser needs to be used.
    
    Also simplifies the parsers to enhance contribution options.

---
+++ src/getContent.js
@@ -0,0 +1,51 @@
+const request = require('request-promise')
+const cheerio = require('cheerio')
+const url = require('url')
+
+const parsers = {
+  abc: require('./parsers/abc'),
+  elmundo: require('./parsers/elmundo'),
+  elpais: require('./parsers/elpais'),
+  lavanguardia: require('./parsers/lavanguardia')
+}
+
+const patterns = {
+  abc: /abc\.es/,
+  elmundo: /elmundo\.es/,
+  elpais: /elpais\.com/,
+  lavanguardia: /lavanguardia\.com/
+}
+
+function getMedia (hostname) {
+  for (const id in patterns) {
+    if (patterns[id].test(hostname)) {
+      return id
+    }
+  }
+  return null
+}
+
+/**
+ * Get the content of an article given its URI
+ *
+ * @param {string} uri - URI of the news article.
+ *
+ * @return {Promise<string>} Promise of the HTML
+ */
+module.exports = function getContent (uri) {
+  const media = getMedia(url.parse(uri).hostname)
+
+  if (!media) {
+    throw new Error('The provided URI do not match with any known media')
+  }
+
+  const options = {
+    transform: function (body) {
+      return cheerio.load(body)
+    },
+    uri
+  }
+
+  return request(options)
+    .then(parsers[media])
+}
