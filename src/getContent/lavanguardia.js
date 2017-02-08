3254d8bfa35ad0c144c702b2bc845a6f36b75598
Carlos Saito <c.saito.style@gmail.com>
2017-02-08 11:45:35 +0100

    Scraper for "El Mundo", "ABC" and "La Vanguardia"

---
+++ src/getContent/lavanguardia.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+module.exports = function getContent(sourceUrl) {
+  return new Promise((accept, reject) => {
+    request(sourceUrl, function (error, response, html) {
+      if (error) {
+        reject(error)
+        return
+      }
+
+      const $ = cheerio.load(html, {
+        normalizeWhitespace: true
+      })
+      const allowedTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+      let content = $('<div></div>')
+
+      $('div[itemprop=articleBody]')
+        .children()
+        .filter(function (i, el) {
+          return allowedTags.indexOf($(this).prop('tagName').toLowerCase()) !== -1
+        })
+        .filter(function (i, el) {
+          return !$(this).hasClass('summary-lead')
+        })
+        .each(function (i, el) {
+          $(content).append($(this))
+        })
+
+      accept(content.html())
+    })
+  })
+}
