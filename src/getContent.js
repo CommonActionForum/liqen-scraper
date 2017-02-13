e08d16f635e2a0d763df5285cccf55cac293e161
Carlos Saito <c.saito.style@gmail.com>
2017-02-13 11:30:06 +0100

    Update "getContent" for further changes

--- src/getContent.js
+++ src/getContent.js
@@ -1,6 +1,7 @@
 const request = require('request-promise')
 const cheerio = require('cheerio')
 const url = require('url')
+const downloadArticle = require('./downloadArticle')
 
 const parsers = {
   abc: require('./parsers/abc'),
@@ -41,6 +42,41 @@ function getMedia (hostname) {
  * @return {Promise<string>} Promise of the HTML
  */
 module.exports = function getContent (uri) {
+  // Decide if use the new "getContent" function or the old one
+  const media = getMedia(url.parse(uri).hostname)
+
+  if (media) {
+    return oldGetContent(uri)
+  } else {
+    return newGetContent(uri)
+  }
+}
+
+/**
+ * @private
+ *
+ * Get the content of an article given its URI
+ *
+ * @param {string} uri - URI of the news article.
+ *
+ * @return {Promise<string>} Promise of the HTML
+ */
+function newGetContent (uri) {
+  return downloadArticle(uri)
+    .then(article => article.html)
+}
+
+/**
+ * @private
+ * @deprecated since version 1.3. This will be replaced with "getContent"
+ *
+ * Get the content of an article given its URI
+ *
+ * @param {string} uri - URI of the news article.
+ *
+ * @return {Promise<string>} Promise of the HTML
+ */
+function oldGetContent (uri) {
   const media = getMedia(url.parse(uri).hostname)
 
   if (!media) {
