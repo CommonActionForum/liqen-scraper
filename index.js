21d866f278add8fb5aef90686e6e37c9d399b2fe
Carlos Saito <c.saito.style@gmail.com>
2017-02-13 12:43:49 +0100

    Fix. Include "downloadArticle" in the API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const downloadArticle = require('./src/downloadArticle')
 const getContent = require('./src/getContent')
 const googleSearch = require('./src/getNewsList/google')
 
 module.exports = {
+  downloadArticle,
   getContent,
   googleSearch
 }
