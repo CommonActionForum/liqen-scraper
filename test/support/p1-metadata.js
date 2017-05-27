f4b7fa8e623f7e04d00d352d2dc0e252fe2995da
Carlos Saito <c.saito.style@gmail.com>
2017-05-27 11:39:07 +0200

    Fix linter errors

--- test/support/p1-metadata.js
+++ test/support/p1-metadata.js
@@ -3,6 +3,6 @@ module.exports = {
   publishedDate: new Date('2017-02-09T00:20:30+01:00'),
   image: 'http://ep00.epimg.net/cultura/imagenes/2017/02/08/actualidad/1486573775_868895_1486574229_rrss_normal.jpg',
   source: {
-    author: 'Rubén Amón',
-  },
+    author: 'Rubén Amón'
+  }
 }
