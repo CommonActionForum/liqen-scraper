285bc749c3ae8555dba17145bd166b78b818a73f
Carlos Saito <c.saito.style@gmail.com>
2017-05-27 13:20:06 +0200

    Fix dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 const downloadArticle = require('./downloadArticle')
-const getContent = require('./getContent')
-const googleSearch = require('./getNewsList/google')
-const advancedSearch = require('./advancedSearch')
+const googleSearch = require('./advancedSearch')
 
 module.exports = {
   downloadArticle,
-  getContent,
-  googleSearch,
-  advancedSearch
+  googleSearch
 }
